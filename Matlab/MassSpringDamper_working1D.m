7553e4f717e446c26ed643a498d3f45bf59ecdd0
Jack Mehmet <jmm546@york.ac.uk>
2017-03-31 13:55:18 +0100

    good sounding string model, ready to produce sounds and graphs

--- Matlab/MassSpringDamper_working1D.m
+++ Matlab/MassSpringDamper_working1D.m
@@ -1,86 +1,118 @@
-% A Single Mass-Spring-Damper Simple Harmonic Oscillator as originally
-% demonstrated in PhyMod
-% DTM 21/01/2016
 
-function Copy_of_MassSpringDamper()
-clc;
-close all; 
+% prepare Matlab for simulation
+clc;       % clear console
+close all; % close all open figures
 
-num_masses = 1000; % mass 1 and num_masses are fixed
+% define audio parameters
+Fs = 44100;                   % sample rate for audio output
+Ts = 1 / Fs;                  % sample period
+seconds = 4;                  % number of seconds of output to be calculated
+Ns = 44100 * seconds;         % number of samples in output
+all_samples_range = (1 : Ns); % range containing all masses
 
-% System Definitions
-Fs = 44100;         % Sample Rate for audio output
-T = 1/Fs;           % Sample period
-% seconds = input('How many seconds would you like me to last? ');
-seconds = 2;
-numSamples = 44100*seconds;          % Number of samples in output
-frequency = 100;
-excitation_index = round(num_masses/2, 0);
-excitation_amplitude = 1;
-      
-xc = zeros(1, num_masses);
-% xc(25:29) = hanning(5)';
-xc(excitation_index) = excitation_amplitude;
-xp = xc;
-xn = zeros(1, num_masses);
+% define mass-spring-damper system parameters
+Nm = 100;                           % total number of masses
+mobile_masses_range = (2 : Nm-1);   % range containing all masses with a mobile mass on each side
+f0 = 1000;                          % resonant freqency of system in Hz
+m = 0.001 * ones(1, Nm);            % mass given in kg
+m_ = [m m(Nm)];                     % length Nm+1 with last element equal to the right-most mass
+k_ = (2*pi*f0)^2 * m_;              % k given in N/m or kg/m*s^2 -> omega0 = 2*pi*f0 = sqrt(k*l/m))
+z_crit = (4 * m_ .* k_).^(-1/2);    % z^2 - 4*m*k = 0 -> http://hyperphysics.phy-astr.gsu.edu/hbase/oscda.html#c1
+z_ = z_crit;                        % z given in N*s/m or kg/m*s
 
-% m = 100 .* ones(1, num_masses) ./ 1000000000; % mass given in kg*10e-9
-% 
-% k =  m .* (2*pi*frequency)^2; % omega0 = sqrt(k/m) = 2*pi*frequency
-%                               % k = m*omega0^2 = m*(2*pi*frequency)^2
-% z = 100 .* ones(1, num_masses) ./ 1000000000; % z given in N*s/(kg*10e-9)
-% 
-% k_ = (k .* T^2) ./ m;
-% z_ = (z .* T) ./ m;
+% scale spring constant and damping factor by appropriate factors to simplify the loop calculation
+k = (k_ ./ m_) * Ts^2;
+z = (z_ ./ m_) * Ts; 
 
-% set all masses, spring constants and damping factors to be equal
-m1 = 100 / 1000000000;
-% k1 = (m1 * (2*pi*frequency)^2) * (T^2 / m1);
-k1 = 10 * (T^2 / m1);
-z1 = (1000 / 1000000000) * (T / m1);
+% define excitation of the system
+Ei = round(Nm/2, 0); % index of center of excitation
+Ea = Nm / 10;         % amplitude of excitation
 
-out = zeros(1, numSamples);
+% initialise the displacements of all masses
+x_next = zeros(1, Nm); % set all next displacements to zero
+x_curr = x_next;       % set all current displacements to zero
+x_curr(Ei) = Ea;       % insert excitation into current displacements
+x_prev = x_curr;       % set previous displacements to be equal to current
 
-% Main Loop
-for n = (1 : numSamples) 
-%     for i = (2 : num_masses - 1)
-%         xn(i) = xc(i) * (k_(i) + z_(i) + k_(i - 1) + z_(i - 1) + 2) - xc(i + 1) * (k_(i) + z_(i)) - xc(i - 1) * (k_(i - 1) + z_(i - 1)) ...
-%               - xp(i) * (z_(i) + z_(i - 1) + 1)                     + xp(i + 1) * z_(i)           + xp(i - 1) * z_(i - 1);
-%     end
-    
-    for i = (2 : num_masses - 1)
-        xn(i) = xc(i) * 2 * (1 - k1 - z1) + (xc(i + 1) + xc(i - 1)) * (k1 + z1) ...
-              - xp(i) * (1 - 2 * z1)      - (xp(i + 1) + xp(i - 1)) * z1;
+% define index of mass whose displacement will be taken as output
+Oi = Nm/5;          % index of mass to use as output
+O = zeros(1, Ns); % output buffer for samples
+
+% initialise loop parameters
+plot_enable = 1;          % enables the real-time plot of the masses' displacement
+plot_update_delay = 0.01; % seconds between updating real-time graph
+dispstat('', 'init');     % initialise dispstat so that % progress is displayed properly
+f0_scale = 2;
+
+% main loop
+for n = all_samples_range
+    if mod(n, Ns/4) == 0
+        x_next = zeros(1, Nm); % set all next displacements to zero
+        x_curr = x_next;       % set all current displacements to zero
+        x_curr(Ei) = Ea;       % insert excitation into current displacements
+        x_prev = x_curr;       % set previous displacements to be equal to current
+        f0 = f0 * f0_scale;
+        k = k * f0_scale;
     end
     
-%     out(n) = xn(excitation_index);
-    out(n) = xn(2);
+    % calculate next displacement of first mass simplified with x(i-1) terms disappearing
+    x_next(1) = x_curr(1) * (2 - k(2) - z(2) - k(1) - z(1)) + x_curr(2) * (k(2) + z(2)) ...
+              - x_prev(1) * (1 -        z(2) -        z(1)) - x_prev(2) *         z(2);
+          
+    % calculate next displacement of middle masses
+    for i = mobile_masses_range
+        x_next(i) = x_curr(i) * (2 - k(i+1) - z(i+1) - k(i) - z(i)) + x_curr(i+1) * (k(i+1) + z(i+1)) + x_curr(i-1) * (k(i) + z(i)) ...
+                  - x_prev(i) * (1 -          z(i+1) -        z(i)) - x_prev(i+1) *           z(i+1)  - x_prev(i-1) *         z(i);
+    end
     
-%     Resamples the output at a rate of Fs/200 to plot the mass displacement in real-time.
-%     if mod(n, 100) == 0   
-%         plot(xn); 
-%         axis([0 num_masses -0.1 0.1]);
-%         pause(0.0001);
-%     end
+    % calculate next displacement of last mass simplified with x(i+1) terms disappearing
+    x_next(Nm) = x_curr(Nm) * (2 - k(Nm+1) - z(Nm+1) - k(Nm) - z(Nm)) + x_curr(Nm-1) * (k(Nm) + z(Nm)) ...
+               - x_prev(Nm) * (1 - z(Nm+1) -                   z(Nm)) - x_prev(Nm-1) *          z(Nm);
     
-    xp = xc;
-    xc = xn;
+    % set output sample to new displacement of the output mass
+    O(n) = x_next(Oi);
+    
+    % plot the mass displacement in real-time
+    if (plot_enable == 1) && (mod(n, Fs*plot_update_delay) == 0)
+        subplot(2,1,1), plot(x_next); 
+        axis([1 Nm -Ea/4 Ea/4]), xlabel('Mass'), ylabel('Amplitude');
+        title(sprintf('m=%fkg, k=%fN/m, z=%fN*s/m, f0=%dHz', m(1), k(1), z(1), f0));
+        subplot(2,1,2), plot(O); 
+        axis([1 Ns -Ea/4 Ea/4]), xlabel('Sample'), ylabel(sprintf('Amplitude of mass %d', Oi));
+        pause(plot_update_delay);
+    end
+    
+    % display progress of simulation in percent
+    if mod(n, Ns/100) == 0
+        dispstat(sprintf('%d%%', 100*n/Ns));
+    end
+    
+    % set up arrays for next iteration
+    x_prev = x_curr;
+    x_curr = x_next;
 end
+
+% check whether there are any output samples
+if abs(max(O)) > 0 
+    Onorm = O / max(O);
     
-% Plot time domain response of output
-figure(2);
-plot(out);
-xlabel('Time (samples)');
-ylabel('Amplitude');
+    % Plot time domain response of output
+    figure(2);
+    plot(Onorm);
+    xlabel('Time (samples)');
+    ylabel('Amplitude');
 
-fftSize = 8192;
-f = (0:fftSize-1)*(Fs/fftSize);
+    fftSize = 8192;
+    f = (0:fftSize-1)*(Fs/fftSize);
 
-% Plot frequency domain response of output
-figure(3);
-semilogx(f,20*log10(abs(fft(out,fftSize))/max(abs(fft(out,fftSize)))));
-axis([0 8000 -40 0]);
-xlabel('Frequency (Hz)');
-ylabel('Magnitude Response (dB)');
+    % Plot frequency domain response of output
+    figure(3);
+    semilogx(f, 20 * log10(abs(fft(Onorm, fftSize)) / max(abs(fft(Onorm, fftSize)))));
+    axis([0 8000 -40 0]);
+    xlabel('Frequency (Hz)');
+    ylabel('Magnitude Response (dB)');
 
-sound(out ./ max(out), Fs);
+    sound(Onorm, Fs);
+else
+    disp('All output samples are 0');
+end
